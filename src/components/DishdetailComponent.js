121453611a6de2163c0e0303dc036831e31adf03
David Calle <dcalleg@eafit.edu.co>
2021-01-02 17:19:55 -0500

    functional components

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,44 +1,54 @@
-import React, { Component } from 'react';
-import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+import React from 'react';
+import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 
-class DishDetail extends Component{
 
-    render(){
-        const dish = this.props.dish;
-        if(dish != null){
-            const comments = dish.comments.map((comment) => {
-                return(
-                    <div key={comment.id}>
-                        <p>{comment.comment}</p>
-                        <p>-- {comment.author}, { new Intl.DateTimeFormat('en-US', {year: 'numeric', month:'short', day:'2-digit'}).format(new Date( Date.parse(comment.date)))}</p>
-                    </div>)
-                    });
-            return(
-                <div className="container">
-                    <div className="row">
-                        <div className="col-12 col-md-5 m-1">
-                            <Card>
-                                <CardImg width="100%" src={dish.image} alt={dish.name}/>
-                                <CardBody>
-                                    <CardTitle>{dish.name}</CardTitle>
-                                    <CardText>{dish.description}</CardText>
-                                </CardBody>
-                            </Card>
-                        </div>
-                        <div className="col-12 col-md-5 m-1">
-                            <h2>Comments</h2>
-                            {comments}
-                        </div>
-                    </div> 
-                </div>
-                
-            );
-        }
-        else{
-            return(<div></div>);
-        }
-            
+function RenderDish({dish}){
+    return(
+        <div className="col-12 col-md-5 m-1">
+            <Card>
+                <CardImg width="100%" src={dish.image} alt={dish.name}/>
+                <CardBody>
+                    <CardTitle>{dish.name}</CardTitle>
+                    <CardText>{dish.description}</CardText>
+                </CardBody>
+            </Card>
+        </div>
+    );  
+}
+
+const RenderComents = ({comments}) => {
+    const coms = comments.map((comment) => {
+        return(
+            <div key={comment.id}>
+                <p>{comment.comment}</p>
+                <p>-- {comment.author}, { new Intl.DateTimeFormat('en-US', {year: 'numeric', month:'short', day:'2-digit'}).format(new Date( Date.parse(comment.date)))}</p>
+            </div>)
+    });
+    return(
+        <div className="col-12 col-md-5 m-1">
+            <h2>Comments</h2>
+            {coms}
+        </div>
+    )
+
+}
+
+const DishDetail = (props) => {
+    const dish = props.dish;
+    if(dish != null){   
+        return(
+            <div className="container">
+                <div className="row">
+                    <RenderDish dish={dish}/>
+                    <RenderComents comments={dish.comments}/>
+                </div> 
+            </div>  
+        );
+    }
+    else{
+        return(<div></div>);
     }
 }
+
 
 export default DishDetail;
