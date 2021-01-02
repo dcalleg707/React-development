9ad789168c0fef81eaac49916281450ff2828426
David Calle <dcalleg@eafit.edu.co>
2021-01-02 14:09:29 -0500

    Presentational and container components

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -5,31 +5,39 @@ class DishDetail extends Component{
 
     render(){
         const dish = this.props.dish;
-        const comments = dish.comments.map((comment) => {
+        if(dish != null){
+            const comments = dish.comments.map((comment) => {
+                return(
+                    <div key={comment.id}>
+                        <p>{comment.comment}</p>
+                        <p>-- {comment.author}, { new Intl.DateTimeFormat('en-US', {year: 'numeric', month:'short', day:'2-digit'}).format(new Date( Date.parse(comment.date)))}</p>
+                    </div>)
+                    });
             return(
-                <div key={comment.id}>
-                    <p>{comment.comment}</p>
-                    <p>-- {comment.author}, {comment.date}</p>
-                </div>)
-                });
-
-            return(
-                <div className="row">
-                <div className="col-12 col-md-5 m-1">
-                    <Card>
-                        <CardImg width="100%" src={dish.image} alt={dish.name}/>
-                        <CardBody>
-                            <CardTitle>{dish.name}</CardTitle>
-                            <CardText>{dish.description}</CardText>
-                        </CardBody>
-                    </Card>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-12 col-md-5 m-1">
+                            <Card>
+                                <CardImg width="100%" src={dish.image} alt={dish.name}/>
+                                <CardBody>
+                                    <CardTitle>{dish.name}</CardTitle>
+                                    <CardText>{dish.description}</CardText>
+                                </CardBody>
+                            </Card>
+                        </div>
+                        <div className="col-12 col-md-5 m-1">
+                            <h2>Comments</h2>
+                            {comments}
+                        </div>
+                    </div> 
                 </div>
-                <div className="col-12 col-md-5 m-1">
-                    <h2>Comments</h2>
-                    {comments}
-                </div>
-            </div> 
+                
             );
+        }
+        else{
+            return(<div></div>);
+        }
+            
     }
 }
 
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,35 +1,16 @@
 import React, { Component } from 'react';
 import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
-import DishDetail from './DishdetailComponent';
-
 class Menu extends Component{
 
     constructor(props){
         super(props);
-
-        this.state= {
-            selectedDish: null
-        }
-    }
-
-    onDishSelect(dish){
-        this.setState({selectedDish: dish});
-    }
-
-    renderDish(dish){
-        if (dish != null){
-            return(<DishDetail dish={dish}/>);
-        }
-        else{
-            return (<div></div>);
-        }
     }
 
     render(){
         const menu = this.props.dishes.map((dish) => {
             return(
-                <div key={dish.id} className="col-12 col-md-5 m-1" onClick={() => this.onDishSelect(dish)}>
-                    <Card>
+                <div key={dish.id} className="col-12 col-md-5 m-1" >
+                    <Card onClick={() => this.props.onClick(dish.id)}>
                         <CardImg width="100%" src={dish.image} alt={dish.name}/>
                         <CardImgOverlay>
                             <CardTitle>{dish.name}</CardTitle>
@@ -45,7 +26,6 @@ class Menu extends Component{
                 <div className="row">
                     {menu}
                 </div>
-                {this.renderDish(this.state.selectedDish)}
             </div>
         );
     }
